361d98d73e18543ddab697a25a73a054db2f7f0f
BrilliantKimari <brilliantkimari5@gmail.com>
2025-08-12 09:48:32 +0300

    Inheritance

--- classes/inheritance.py
+++ classes/inheritance.py
@@ -1,20 +1,31 @@
 class Shape:
 
 
-    def __init__(self,name,radius):
+    def __init__(self,name):
         self.name=name
 
     def describe(self):
         print(f"This is a shape called {self.name}")
 
+
+
+shape1=Shape(name="Circle")
+shape1.describe()
+
+
+class Circle(Shape):
+
+    def __init__(self,radius):
+        super().__init__(name="Circle")
+        self.radius = radius
+
     def area(self):
         a = 3.142 * self.radius * self.radius
-        print(f"The area of the {self.name} is {a}")
+        print(f"The area of the circle is {a}")
 
-
-shape1=Shape(name="Circle",5)
-shape1.area()
-shape1.describe()
+c1 = Circle(radius=5)
+c1.describe()
+c1.area()
 
 
 class Rectangle(Shape):
