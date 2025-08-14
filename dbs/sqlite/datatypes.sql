077f264542665889e63515291ffd7c930f50b002
BrilliantKimari <brilliantkimari5@gmail.com>
2025-08-14 10:32:58 +0300

    sql datatype inserting

--- dbs/sqlite/datatypes.sql
+++ dbs/sqlite/datatypes.sql
@@ -1,6 +1,3 @@
-
--- FIRST COLUMN -> unique
---sql SQL SYNTAX variable in small
 CREATE TABLE IF NOT EXISTS student(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
@@ -8,4 +5,9 @@ CREATE TABLE IF NOT EXISTS student(
     dob TEXT,
     phone INTEGER,
     marks REAL
-)
+);
+
+INSERT INTO student (name,email,dob,phone,marks)
+VALUES ('alice','alice@gmail.com','2002-05-14',254732323,88.2);
+
+SELECT * FROM student
